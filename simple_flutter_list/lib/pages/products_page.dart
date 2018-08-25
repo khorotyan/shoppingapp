b57ebed07ea1975b400a37757dc8ac2b8c96087c
Vahagn Khorotyan <khorotyanvahagn@gmail.com>
2018-08-25 21:28:13 +0400

    Add a drawer to the products page

--- simple_flutter_list/lib/pages/products_page.dart
+++ simple_flutter_list/lib/pages/products_page.dart
@@ -6,6 +6,16 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('Home page')), body: ProductManager());
+        // drawer is on the left, endDrawer on the right
+        drawer: Drawer(
+            child: Column(children: <Widget>[
+          AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
+          ListTile(
+            title: Text('Manage Products'),
+            onTap: () {},
+          )
+        ])),
+        appBar: AppBar(title: Text('Home page')),
+        body: ProductManager());
   }
 }
