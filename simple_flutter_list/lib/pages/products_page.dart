4aa5b7bb4997f0027503de0026a17f7c9ad46eb2
Vahagn Khorotyan <khorotyanvahagn@gmail.com>
2018-08-25 21:46:28 +0400

    Drawer to go to the products page from the manage products page

--- simple_flutter_list/lib/pages/products_page.dart
+++ simple_flutter_list/lib/pages/products_page.dart
@@ -10,11 +10,12 @@ class ProductsPage extends StatelessWidget {
         // drawer is on the left, endDrawer on the right
         drawer: Drawer(
             child: Column(children: <Widget>[
+              // automaticallyImplyLeading disables the drawer icon
           AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
           ListTile(
             title: Text('Manage Products'),
             onTap: () {
-              Navigator.push(
+              Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(
                       builder: (BuildContext context) => ProductsAdminPage()));
