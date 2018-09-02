f980b9b8565f880f6fd50bebfebe4522ffe38a6c
Vahagn Khorotyan <khorotyanvahagn@gmail.com>
2018-09-02 17:54:22 +0400

    Input added to the product creation page

--- simple_flutter_list/lib/pages/product_create_page.dart
+++ simple_flutter_list/lib/pages/product_create_page.dart
@@ -1,29 +1,54 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatelessWidget {
+class ProductCreatePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ProductCreatePageState();
+  }
+}
+
+class _ProductCreatePageState extends State<ProductCreatePage> {
+  String title = '';
+  String description = '';
+  double price = 0.0;
+
   @override
   Widget build(BuildContext context) {
     // Return the body of the page, because this is a tab
     //  no need for a Scaffold and appBar
-    return Center(
-        child: RaisedButton(
-            color: Theme.of(context).accentColor,
-            child: Text('Save Product', style: TextStyle(color: Colors.white)),
-            onPressed: () {
-              showModalBottomSheet(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Image.asset('images/img512_512.png',
-                            width: 256.0, height: 256.0),
-                        Container(
-                            padding: EdgeInsets.all(12.0),
-                            child: Text('Product created!'))
-                      ],
-                    );
-                  });
-            }));
+    return Container(
+        margin: EdgeInsets.all(12.0),
+        child: ListView(children: <Widget>[
+          TextField(
+              decoration: InputDecoration(labelText: 'Product Title'),
+              onChanged: (String value) {
+                setState(() {
+                  title = value;
+                });
+              }),
+          TextField(
+              decoration: InputDecoration(labelText: 'Product Description'),
+              maxLines: 5,
+              onChanged: (String value) {
+                setState(() {
+                  description = value;
+                });
+              }),
+          TextField(
+              decoration: InputDecoration(labelText: 'Product Price'),
+              keyboardType: TextInputType.number,
+              onChanged: (String value) {
+                setState(() {
+                  price = double.parse(value);
+                });
+              }),
+          Container(
+              padding: EdgeInsets.only(top: 10.0),
+              child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('Create Product',
+                      style: TextStyle(color: Colors.white)),
+                  onPressed: () {}))
+        ]));
   }
 }
